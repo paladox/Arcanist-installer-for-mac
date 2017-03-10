df3fa91648b2ffc392b6b5cfa5b3ee69ddd5158c
Paladox <thomasmulhall410@yahoo.com>
2017-03-10 22:09:27 +0000

    Update arcanist and libphutil to upstream
    Also update version to 1.1.
    
    Change-Id: I02f47fe2cdbcc4d336f7903b8cab8289c84a797d

--- uninstaller/uninstaller.sh
+++ uninstaller/uninstaller.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pkgbuild --root ../Arcanist --scripts scripts --identifier com.arcanist.arc --version 1 --install-location /private/var/arc-temp arcanist-uninstaller.pkg
+pkgbuild --root ../Arcanist --scripts scripts --identifier com.arcanist.arc --version 1.1 --install-location /private/var/arc-temp arcanist-uninstaller.pkg
